fa9f66832466c6ff16fa3644592b2c8518f5018a
Bighneshi <74350221+Bighneshi@users.noreply.github.com>
2021-05-10 15:46:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,16 +12,23 @@ recognition.onresult = function(event){
     var content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
     console.log(content);
-    speak();
-
+    if(content == "take my selfie"){
+        console.log("Taking Selfie---"); 
+        speak();
+    }
+   
 }   
 
 function speak() {
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking Your Selfie in 5 Seconds"
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
 } 
 
 Webcam.set({
@@ -33,3 +40,16 @@ Webcam.set({
 });
 
 camera = document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "selfie_img" src = "'+ data_uri+ '"/>';
+    });
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+   }
